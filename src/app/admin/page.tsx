9ef3fc07cc793c47c14e316192b21c4a1d524db2
Otavio Henrique <hotavio.silva11@gmail.com>
2025-07-25 16:02:01 -0300

    acrescentei 1 virgula

--- src/app/admin/page.tsx
+++ src/app/admin/page.tsx
@@ -20,7 +20,7 @@ const amarelo = "#D4B233";
 const adminEmails = [
   "suporte@postos4ilhas.com.br",
   "guilherme@postos4ilhas.com.br",
-  "compras@postos4ilhas.com.br"
+  "compras@postos4ilhas.com.br",
 ];
 
 export default function AdminPage() {
