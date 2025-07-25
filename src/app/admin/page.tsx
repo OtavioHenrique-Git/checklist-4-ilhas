de6e9bd16cd88dfe5a1d916e68ad7b3ac883748f
Otavio Henrique <hotavio.silva11@gmail.com>
2025-07-25 15:56:56 -0300

    acrescentar mais um email do compras

--- src/app/admin/page.tsx
+++ src/app/admin/page.tsx
@@ -20,7 +20,7 @@ const amarelo = "#D4B233";
 const adminEmails = [
   "suporte@postos4ilhas.com.br",
   "guilherme@postos4ilhas.com.br",
-  "admin@4ilhas.com"
+  "compras@postos4ilhas.com.br"
 ];
 
 export default function AdminPage() {
