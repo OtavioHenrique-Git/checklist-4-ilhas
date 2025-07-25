4a13fca55da4192ee8adcdc64ea5fe3e09acdbf8
Otavio Henrique <hotavio.silva11@gmail.com>
2025-07-25 10:31:54 -0300

    Força alteração no next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,10 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
+  // Se quiser adicionar mais configs, coloque aqui
 };
 
 export default nextConfig;
